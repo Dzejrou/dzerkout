ea249708a034384199b16f451aa043b4b894d562
Jaroslav Jindrak <dzejrou@gmail.com>
2026-04-27 13:04:21 +0200

    Track Android platform project

---
+++ src-tauri/gen/android/app/src/main/java/com/dzerkout/app/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.dzerkout.app
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+    hideSystemBars()
+  }
+
+  override fun onResume() {
+    super.onResume()
+    hideSystemBars()
+  }
+
+  override fun onWindowFocusChanged(hasFocus: Boolean) {
+    super.onWindowFocusChanged(hasFocus)
+    if (hasFocus) hideSystemBars()
+  }
+
+  private fun hideSystemBars() {
+    WindowCompat.getInsetsController(window, window.decorView).apply {
+      hide(WindowInsetsCompat.Type.systemBars())
+      systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+    }
+  }
+}
